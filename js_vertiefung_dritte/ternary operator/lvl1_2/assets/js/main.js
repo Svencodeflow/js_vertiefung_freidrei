31ea16a9c81e164948d7dabc87a419f46f26296a
Svencodeflow <schlaakboot@gmail.com>
2023-03-09 14:33:17 +0100

    color change

--- js_vertiefung_dritte/ternary operator/lvl1_2/assets/js/main.js
+++ js_vertiefung_dritte/ternary operator/lvl1_2/assets/js/main.js
@@ -1,9 +1,18 @@
 
 const passw = document.getElementById("pass");
 
-
 function save() {
-    let pass1 = passw.value
-    console.log(pass1.length);
-    pass1.length >= 8 ? document.getElementById("lang").innerHTML = "Passwort ist lang genug! Willkommen" : document.getElementById("short").innerHTML = "Passwort ist zu kurz";
+    let pass1 = passw.value;
+    let text;
+    let farbe = document.getElementById("short_lang")
+    pass1.length >= 8 ? text = "Passwort ist lang genug! Willkommen" : text = "Passwort ist zu kurz";
+    if (pass1.length >= 8) {
+        farbe.style.color = "green"
+    }else {
+        text
+        farbe.style.color = "red"
+    }
+    document.getElementById("short_lang").innerHTML = text
 }
+
+
